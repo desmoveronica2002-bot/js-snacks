fe1c0a125db39a3a011846e64fa98dca8db8da66
Veronica Golinelli <desmoveronica2002@gmail.com>
2026-02-05 12:10:39 +0100

    push2

--- snack 3/script.js
+++ snack 3/script.js
@@ -1,8 +1,8 @@
-let s=0;
+let sum=0;
 
 for(let i=0; i<11; i++){
-   let n = Number(prompt("numero"));
-   s= s + n; // s=0 quindi 0+n
+   let num = Number(prompt("numero"));
+   sum= sum + num; // s=0 quindi 0+n
 }
 
-console.log("somma:", s);
+console.log("somma:", sum);
