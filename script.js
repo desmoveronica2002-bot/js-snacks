3f4bdd34c120208af0fccd690a805554101bfb9c
Veronica Golinelli <desmoveronica2002@gmail.com>
2026-02-05 10:42:44 +0100

    finish

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
-let N1 = prompt('N1');
-let N2 = prompt('N2');
+let N1 = Number(prompt('N1'));
+let N2 = Number(prompt('N2'));
+
+if(N1<N2){
+    console.log(N2)
+}
+else if(N1>N2){
+    console.log(N1)
+}
+else {
+    console.log(N1,N2)
+}
